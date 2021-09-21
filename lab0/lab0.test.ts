5350d9d46aadbbb14c194ace636aa47ef56b0ce7
Alex Kurochkin <kurochkin@targetprocess.com>
2021-09-21 20:33:47 +0300

    lab1

--- lab0/lab0.test.ts
+++ lab0/lab0.test.ts
@@ -3,14 +3,16 @@ import {helloWorld, typescriptIntro} from './lab0'
 describe('lab0', () => {
   it('should print hello world using console.log', () => {
     const originalConsoleLog = console.log
-    const mockedConsoleLog = jest.fn()
-    console.log = mockedConsoleLog
+    try {
+      const mockedConsoleLog = jest.fn()
+      console.log = mockedConsoleLog
 
-    helloWorld()
+      helloWorld()
 
-    expect(mockedConsoleLog).toHaveBeenCalledWith('Hello, world!')
-
-    console.log = originalConsoleLog
+      expect(mockedConsoleLog).toHaveBeenCalledWith('Hello, world!')
+    } finally {
+      console.log = originalConsoleLog
+    }
   })
 
   it('should execute typescript intro to the end', () => {
